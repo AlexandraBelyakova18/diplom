229f902079c7839f933800c7a860e58439e3fec5
AlexandraBelyakova18 <165970848+AlexandraBelyakova18@users.noreply.github.com>
2025-04-12 23:02:36 +0300

    [

--- OnlineLearningSystem/Models/Answer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineLearningSystem.Models
-{
-    public class Answer
-    {
-        public string Text { get; set; } // Текст ответа
-        public bool IsCorrect { get; set; } // Является ли ответ правильным
-    }
-}
