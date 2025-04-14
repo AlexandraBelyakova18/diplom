daef7261782fdb45def6f0b4386373704a9527d1
AlexandraBelyakova18 <165970848+AlexandraBelyakova18@users.noreply.github.com>
2025-04-14 18:30:56 +0300

    unit

---
+++ OnlineLearningSystem.Tests/ContentAdvisor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineLearningSystem
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int DifficultyLevel { get; set; }
+    }
+
+    public class ContentAdvisor
+    {
+        private List<Material> materials;
+
+        public ContentAdvisor(List<Material> materialList)
+        {
+            materials = materialList;
+        }
+
+        // Метод возвращает список материалов на основе уровня знаний пользователя
+        public List<Material> GetRecommendedMaterials(int knowledgeLevel)
+        {
+            return materials.Where(m => m.DifficultyLevel <= knowledgeLevel).ToList();
+        }
+    }
+}
---
+++ OnlineLearningSystem.Tests/Test1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace OnlineLearningSystem.Tests
+{
+    [TestClass]
+    public class Test1
+    {
+        private ContentAdvisor advisor;
+
+        // Данные для тестов
+        private List<Material> materials;
+
+        [TestInitialize] // Этот метод будет выполняться перед каждым тестом
+        public void Setup()
+        {
+            materials = new List<Material>
+            {
+                new Material { Id = 1, Title = "Easy Material", DifficultyLevel = 1 },
+                new Material { Id = 2, Title = "Medium Material", DifficultyLevel = 2 },
+                new Material { Id = 3, Title = "Hard Material", DifficultyLevel = 3 },
+                new Material { Id = 4, Title = "Very Hard Material", DifficultyLevel = 4 }
+            };
+
+            advisor = new ContentAdvisor(materials);
+        }
+
+        [TestMethod]
+        public void GetRecommendedMaterials_WithMultipleMaterials_ReturnsCorrectMaterials()
+        {
+            // Arrange
+            var userKnowledgeLevel = 2;
+
+            // Act
+            var recommendedMaterials = advisor.GetRecommendedMaterials(userKnowledgeLevel);
+
+            // Assert
+            Assert.AreEqual(2, recommendedMaterials.Count);
+            Assert.IsTrue(recommendedMaterials.Exists(m => m.Id == 1));
+            Assert.IsTrue(recommendedMaterials.Exists(m => m.Id == 2));
+            Assert.IsFalse(recommendedMaterials.Exists(m => m.Id == 3));
+        }
+
+        [TestMethod]
+        public void GetRecommendedMaterials_WithNoMaterials_ReturnsEmptyList()
+        {
+            // Arrange
+            var emptyAdvisor = new ContentAdvisor(new List<Material>());
+            var userKnowledgeLevel = 1;
+
+            // Act
+            var recommendedMaterials = emptyAdvisor.GetRecommendedMaterials(userKnowledgeLevel);
+
+            // Assert
+            Assert.AreEqual(0, recommendedMaterials.Count);
+        }
+
+        [TestMethod]
+        public void GetRecommendedMaterials_WithAllMaterialsHard_ReturnsEmptyList()
+        {
+            // Arrange
+            var userKnowledgeLevel = 2;
+            var hardMaterials = new List<Material>
+            {
+                new Material { Id = 3, Title = "Hard Material", DifficultyLevel = 3 },
+                new Material { Id = 4, Title = "Very Hard Material", DifficultyLevel = 4 }
+            };
+            var hardAdvisor = new ContentAdvisor(hardMaterials);
+
+            // Act
+            var recommendedMaterials = hardAdvisor.GetRecommendedMaterials(userKnowledgeLevel);
+
+            // Assert
+            Assert.AreEqual(0, recommendedMaterials.Count);
+        }
+    }
+}
